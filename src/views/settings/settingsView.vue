404cd0e61e1a5b748b161e7b0f82575364bc0c7c
Kapitan Nemo <lasota020@gmail.com>
2022-12-26 10:24:34 +0100

    Settings icon - need refactor

--- src/views/settings/settingsView.vue
+++ src/views/settings/settingsView.vue
@@ -1,29 +1,36 @@
 <script setup lang="ts">
-import { ref } from "vue";
 import Header from "@/components/headerBar.vue";
-const avatars = import.meta.glob("@/assets/icons/avatars/*.svg");
-const avatarsArray = Object.keys(avatars).map((font) =>
-  font.replace("@/assets/icons/avatars/", "")
-);
-const currentAvatar = ref("/src/assets/icons/avatars/avatar-1.svg"); //TODO: get from api call
-
-const activeAvatar = (avatar: string) => {
-  currentAvatar.value = avatar;
-};
+//TODO: refactor
+// const avatars = import.meta.glob("@/assets/icons/avatars/*.svg");
+// const avatarsArray = Object.keys(avatars).map((avatar) =>
+//   avatar.replace("@/assets/icons/avatars/", "")
+// );
+// const currentAvatar = ref("/assets/icons/avatars/avatar-1.svg"); //TODO: get from api call
+// const activeAvatar = (avatar: string) => {
+//   currentAvatar.value = avatar;
+// };
+import Avatar from "@/assets/icons/avatars/avatar-1.svg?component";
 </script>
 
 <template>
   <Header msg="Select your fighter"></Header>
   <div class="avatars">
-    <div
-      class="avatars__single"
-      @click="activeAvatar(item)"
-      v-for="item in avatarsArray"
-      :key="item"
-    >
-      <img class="avatars__single-photo" :src="`${item}`" />
-      <p class="avatars__single-active" v-if="currentAvatar == item">Active</p>
+    <div class="avatars__single">
+      <avatar class="avatars__single-photo" />
+      <p class="avatars__single-active">Active</p>
     </div>
+    <!-- //TODO: refactor -->
+    <!--  <div
+      class="avatars__single"
+      @click="activeAvatar(avatar)"
+      v-for="avatar in avatarsArray"
+      :key="avatar"
+    >
+      <img class="avatars__single-photo" :src="`${avatar}`" />
+      <p class="avatars__single-active" v-if="currentAvatar == avatar">
+        Active
+      </p>
+    </div> -->
   </div>
 </template>
 
