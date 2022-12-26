a066ce679b18b58db371dccbde21917d9fffb045
Kapitan Nemo <lasota020@gmail.com>
2022-12-26 17:17:59 +0100

    global component for import svg avatar

---
+++ src/components/avatarIcon.vue
@@ -0,0 +1,19 @@
+<template>
+  <component :is="icon" />
+</template>
+
+<script lang="ts" setup>
+import { defineAsyncComponent } from "vue";
+
+const props = defineProps({
+  name: {
+    type: String,
+    required: true,
+  },
+});
+
+const icon = defineAsyncComponent(
+  () => import(`@/assets/icons/avatars/${props.name}.svg?component`)
+);
+console.log(icon);
+</script>
--- src/views/settings/settingsView.vue
+++ src/views/settings/settingsView.vue
@@ -1,36 +1,36 @@
 <script setup lang="ts">
 import Header from "@/components/headerBar.vue";
+import icon from "@/components/avatarIcon.vue";
+import { ref } from "vue";
+
 //TODO: refactor
-// const avatars = import.meta.glob("@/assets/icons/avatars/*.svg");
-// const avatarsArray = Object.keys(avatars).map((avatar) =>
-//   avatar.replace("@/assets/icons/avatars/", "")
-// );
-// const currentAvatar = ref("/assets/icons/avatars/avatar-1.svg"); //TODO: get from api call
-// const activeAvatar = (avatar: string) => {
-//   currentAvatar.value = avatar;
-// };
-import Avatar from "@/assets/icons/avatars/avatar-1.svg?component";
+const avatars = import.meta.glob("@/assets/icons/avatars/*.svg");
+const avatarsArray = Object.keys(avatars).map((avatar) =>
+  avatar.replace("/src/assets/icons/avatars/", "").replace(".svg", "")
+);
+
+console.log(avatarsArray);
+const currentAvatar = ref("avatar-1"); //TODO: get from api call
+const activeAvatar = (avatar: string) => {
+  currentAvatar.value = avatar;
+};
 </script>
 
 <template>
   <Header msg="Select your fighter"></Header>
   <div class="avatars">
-    <div class="avatars__single">
-      <avatar class="avatars__single-photo" />
-      <p class="avatars__single-active">Active</p>
-    </div>
     <!-- //TODO: refactor -->
-    <!--  <div
+    <div
       class="avatars__single"
       @click="activeAvatar(avatar)"
       v-for="avatar in avatarsArray"
       :key="avatar"
     >
-      <img class="avatars__single-photo" :src="`${avatar}`" />
+      <icon class="avatars__single-photo" :name="avatar" />
       <p class="avatars__single-active" v-if="currentAvatar == avatar">
         Active
       </p>
-    </div> -->
+    </div>
   </div>
 </template>
 
